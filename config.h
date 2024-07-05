6e68c4f8560e8cf390daedd88dd2132aaf9c04b2
James Garvey <jamesgarvey@me.com>
2024-07-05 20:58:51 +0100

    defined new config bits

--- config.h
+++ config.h
@@ -1,31 +1,69 @@
 #ifndef CONFIG_H
+
     #define CONFIG_H
+
+    // PIC18F05Q40 Configuration Bit Settings
+
+    // 'C' source line config statements
+
+    // CONFIG1
+    #pragma config FEXTOSC = OFF    // External Oscillator Selection (Oscillator not enabled)
+    #pragma config RSTOSC = HFINTOSC_64MHZ// Reset Oscillator Selection (HFINTOSC with HFFRQ = 64 MHz and CDIV = 1:1)
+
+    // CONFIG2
+    #pragma config CLKOUTEN = OFF   // Clock out Enable bit (CLKOUT function is disabled)
+    #pragma config PR1WAY = OFF     // PRLOCKED One-Way Set Enable bit (PRLOCKED bit can be set and cleared repeatedly)
+    #pragma config CSWEN = ON       // Clock Switch Enable bit (Writing to NOSC and NDIV is allowed)
+    #pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
+    #pragma config FCMENP = OFF     // Fail-Safe Clock Monitor - Primary XTAL Enable bit (Fail-Safe Clock Monitor disabled)
+    #pragma config FCMENS = OFF     // Fail-Safe Clock Monitor - Secondary XTAL Enable bit (Fail-Safe Clock Monitor Disabled)
+
+    // CONFIG3
+    #pragma config MCLRE = EXTMCLR  // MCLR Enable bit (If LVP = 0, MCLR pin is MCLR; If LVP = 1, RE3 pin function is MCLR )
+    #pragma config PWRTS = PWRT_64  // Power-up timer selection bits (PWRT set at 64ms)
+    #pragma config MVECEN = ON      // Multi-vector enable bit (Multi-vector enabled, Vector table used for interrupts)
+    #pragma config IVT1WAY = OFF    // IVTLOCK bit One-way set enable bit (IVTLOCKED bit can be cleared and set repeatedly)
+    #pragma config LPBOREN = ON     // Low Power BOR Enable bit (Low-Power BOR enabled)
+    #pragma config BOREN = SBORDIS  // Brown-out Reset Enable bits (Brown-out Reset enabled , SBOREN bit is ignored)
+
+    // CONFIG4
+    #pragma config BORV = VBOR_2P85 // Brown-out Reset Voltage Selection bits (Brown-out Reset Voltage (VBOR) set to 2.8V)
+    #pragma config ZCD = OFF        // ZCD Disable bit (ZCD module is disabled. ZCD can be enabled by setting the ZCDSEN bit of ZCDCON)
+    #pragma config PPS1WAY = OFF    // PPSLOCK bit One-Way Set Enable bit (PPSLOCKED bit can be set and cleared repeatedly (subject to the unlock sequence))
+    #pragma config STVREN = ON      // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
+    #pragma config LVP = ON         // Low Voltage Programming Enable bit (Low voltage programming enabled. MCLR/VPP pin function is MCLR. MCLRE configuration bit is ignored)
+    #pragma config XINST = ON       // Extended Instruction Set Enable bit (Extended Instruction Set and Indexed Addressing Mode enabled)
+
+    // CONFIG5
+    #pragma config WDTCPS = WDTCPS_14// WDT Period selection bits (Divider ratio 1:524299)
+    #pragma config WDTE = SWDTEN    // WDT operating mode (WDT enabled/disabled by SWDTEN bit)
+
+    // CONFIG6
+    #pragma config WDTCWS = WDTCWS_7// WDT Window Select bits (window always open (100%); software control; keyed access not required)
+    #pragma config WDTCCS = LFINTOSC// WDT input clock selector (WDT reference clock is the 31.0 kHz LFINTOSC)
+
+    // CONFIG7
+    #pragma config BBSIZE = BBSIZE_512// Boot Block Size selection bits (Boot Block size is 512 words)
+    #pragma config BBEN = OFF       // Boot Block enable bit (Boot block disabled)
+    #pragma config SAFEN = OFF      // Storage Area Flash enable bit (SAF disabled)
+    #pragma config DEBUG = ON       // Background Debugger (Background Debugger enabled)
+
+    // CONFIG8
+    #pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot Block not Write protected)
+    #pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers not Write protected)
+    #pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM not Write protected)
+    #pragma config WRTSAF = OFF     // SAF Write protection bit (SAF not Write Protected)
+    #pragma config WRTAPP = OFF     // Application Block write protection bit (Application Block not write protected)
+
+    // CONFIG9
+    #pragma config CP = OFF         // PFM and Data EEPROM Code Protection bit (PFM and Data EEPROM code protection disabled)
+
+    // #pragma config statements should precede project file includes.
+    // Use project enums instead of #define for ON and OFF.
 
     #include <xc.h>
     #include <stdio.h>
     #include <math.h>
-
-    // CONFIG1
-    #pragma config FOSC = INTOSC    // Oscillator Selection (INTOSC oscillator: I/O function on CLKIN pin)
-    #pragma config WDTE = OFF       // Watchdog Timer Enable (WDT disabled)
-    #pragma config PWRTE = OFF      // Power-up Timer Enable (PWRT disabled)
-    #pragma config MCLRE = ON       // MCLR Pin Function Select (MCLR/VPP pin function is MCLR)
-    #pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
-    #pragma config CPD = OFF        // Data Memory Code Protection (Data memory code protection is disabled)
-    #pragma config BOREN = OFF      // Brown-out Reset Enable (Brown-out Reset disabled)
-    #pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
-    #pragma config IESO = OFF       // Internal/External Switchover (Internal/External Switchover mode is disabled)
-    #pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable (Fail-Safe Clock Monitor is disabled)
-
-    // CONFIG2
-    #pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
-    #pragma config PLLEN = ON      // PLL Enable (4x PLL disabled)
-    #pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
-    #pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
-    #pragma config DEBUG = ON       // In-Circuit Debugger Mode (In-Circuit Debugger enabled, ICSPCLK and ICSPDAT are dedicated to the debugger)
-    #pragma config LVP = OFF         // Low-Voltage Programming Enable (Low-voltage programming disabled)
-
-    // #pragma config statements should precede project file includes.
 
     #define _XTAL_FREQ 32000000
 
